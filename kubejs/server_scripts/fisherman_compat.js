54f79cb408bc8959445e1800dd72f201a23a3a5b
Reaxt <24214198+Reaxt@users.noreply.github.com>
2026-01-19 19:03:38 -0500

    partial work, a bunch of cooking recipes. i cant remember whats left.

--- kubejs/server_scripts/fisherman_compat.js
+++ kubejs/server_scripts/fisherman_compat.js
@@ -1,3 +1,8 @@
+ServerEvents.tags('item', event => {
+    event.add("forge:salt", "fishermans_haven:salt")
+    event.add("forge:salt", "refurbished_furniture:sea_salt")
+})
+
 ServerEvents.recipes(event => {
     event.recipes.farmersdelight.cutting(
         "#fishermans_haven:fish",
@@ -13,7 +18,51 @@ ServerEvents.recipes(event => {
             "2x fishermans_haven:crab_meat"
         ]
     )
-    event.remove({output: "fishermans_haven:fish_fillet", type: "minecraft:crafting"})
-    event.remove({output: "fishermans_haven:crab_meat", type:"minecraft:crafting"})
+    event.remove({input: "fishermans_haven:fishermans_knife"});
+    event.remove({output: "fishermans_haven:fishermans_knife"});
+    event.remove({output: "fishermans_haven:fish_stew"})
+    event.remove({output: "fishermans_haven:fish_soup"})
+    event.remove({output: "fishermans_haven:shrimp_soup"})
+    event.remove({output: "fishermans_haven:seafood_soup"})
+    event.remove({output:"fishermans_haven:lobster_stew"})
+    event.campfireCooking( "fishermans_haven:cooked_lobster", "fishermans_haven:lobster", 1, 200);
+
+    event.recipes.farmersdelight.cooking(
+        ["minecraft:potato", "minecraft:carrot", "fishermans_haven:salt", "fishermans_haven:cooked_fish_fillet"],
+        "fishermans_haven:fish_stew",
+        1,
+        7,
+        "minecraft:bowl"
+    )
+        event.recipes.farmersdelight.cooking(
+        ["fishermans_haven:salt", "fishermans_haven:fish_fillet"],
+        "fishermans_haven:fish_soup",
+        1,
+        7,
+        "minecraft:bowl"
+    )
+    event.recipes.farmersdelight.cooking(
+        ["fishermans_haven:shrimp", "fishermans_haven:salt", "minecraft:carrot", "minecraft:beetroot"],
+        "fishermans_haven:shrimp_soup",
+        1,
+        7,
+        "minecraft:bowl"
+    )
+    event.recipes.farmersdelight.cooking(
+        ["fishermans_haven:cooked_fish_fillet", "fishermans_haven:cooked_crab_meat", "minecraft:carrot", "fishermans_haven:salt"],
+        "fishermans_haven:seafood_soup",
+        1,
+        7,
+        "minecraft:bowl"
+    )
+    event.recipes.farmersdelight.cooking(
+        ["minecraft:carrot", "fishermans_haven:cooked_lobster", "minecraft:potato", "fishermans_haven:salt"],
+        "fishermans_haven:lobster_stew",
+        1,
+        7,
+        "minecraft:bowl"
+    )
+
+
 
 })
